6de8b523704510565864db2a42cc0cb6aec624e5
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-10-11 20:40:38 +0200

    clase 11/10

--- Plantilla web/7_pruebaGET.php
+++ Plantilla web/7_pruebaGET.php
@@ -1,8 +1,10 @@
 <html>
+
 <head>
     <title>Esto es el titulo</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="contenedor">
         <div class="primera_caja">
@@ -15,7 +17,7 @@
             <div class="primera_columna">
                 <ul class="listaAct">
                     <li class="act"> <a href="1_hola_mundo_comentarios.php">Probando comentarios</a></li>
-                    <li class="act" > <a href="2_variables y tipos.php"> Variables y tipos</a></li>
+                    <li class="act"> <a href="2_variables y tipos.php"> Variables y tipos</a></li>
                     <li class="act"><a href="3_arrays_bucles.php">Arrays y bucles</a></li>
                     <li class="act"><a href="4_constantes.php">Constantes</a></li>
                     <li class="act"><a href="5_superGlobales.php">Super constantes</a></li>
@@ -25,19 +27,22 @@
 
                 </ul>
             </div>
-            <div class="segunda_columna"> <p>
-                <?php
-                    echo "Hola mundo!";
-                ?>
-                </p></div>
+            <div class="segunda_columna">
+                <p>
+                   <?php
+                   require 'funciones.php';
+                   obtenerVocal('letra');
+                    ?>
+                </p>
+            </div>
             <div class="tercera_columna">assas</div>
-            
+
 
 
         </div>
         <div class="tercera_caja">ccc</div>
         <div class="pieDePagina">Pie de página</div>
-    <div>
+        <div>
 </body>
-</html>
 
+</html>
