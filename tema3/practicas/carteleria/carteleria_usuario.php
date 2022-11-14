8b9945146022d440da726cb739dea0a4157d205d
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-11-14 16:03:14 +0100

    avance portada

--- tema3/practicas/carteleria/carteleria_usuario.php
+++ tema3/practicas/carteleria/carteleria_usuario.php
@@ -34,7 +34,7 @@
                 <p>
               
                 <?php
-                require('carteleria.php');
+                require('peliculas.php');
                 $peli = new Pelicula("Barbie en La princesa y la costurera", "La mítica muñeca Barbie cobra vida en esta moderna versión del cuento de Mark Twain \"El Príncipe y el Mendigo\" sobre un error de identidad y el poder de la amistad. Con la voz de Gisela como Barbie cantando siete temas originales llega la primera película musical de Barbie interpretando además un doble papel.", "imgs/img1.jpg");
                 $peli->pintarPeli();
                 ?>
---
+++ tema3/practicas/carteleria/categorias.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categorias</title>
+    <link rel="stylesheet" href="estilos_categorias.css">
+</head>
+<body>
+
+    <h1>- - - CATEGORÍAS DE NUESTRA CARTELERÍA - - -</h1>
+   
+    <div class="contenedorLista">
+    <ul class="listaCategorias">
+        <li class="generos"> <a href="peliculas.php">Terror</a> </li>
+        <li class="generos"> <a href="peliculas.php">Película infantil</a> </li>
+    </ul>
+    </div>
+</body>
+</html>
