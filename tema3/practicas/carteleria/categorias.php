abece54874b7fe52a229e96c17a75a4a3e4424b2
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-11-18 19:09:20 +0100

    condicional funcionando

--- tema3/practicas/carteleria/categorias.php
+++ tema3/practicas/carteleria/categorias.php
@@ -5,16 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categorias</title>
+  
     <link rel="stylesheet" href="estilos_categorias.css">
 </head>
 <body>
-
     <h1>- - - CATEGORÍAS DE NUESTRA CARTELERÍA - - -</h1>
    
     <div class="contenedorLista">
+        
     <ul class="listaCategorias">
-        <li class="generos"> <a href="peliculas.php">Terror</a> </li>
-        <li class="generos"> <a href="peliculas.php">Película infantil</a> </li>
+        <li class="generos"> <a href="peliculas.php?genero=terror">Terror</a> </li>
+        <li class="generos"> <a href="peliculas.php?genero=barbie">Película infantil</a> </li>
     </ul>
     </div>
 </body>
--- tema3/practicas/carteleria/peliculas.php
+++ tema3/practicas/carteleria/peliculas.php
@@ -6,88 +6,104 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pelicula</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Metal+Mania&family=Sevillana&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="estilos_terror.css">
-   
-  
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Metal+Mania&family=Sevillana&display=swap" rel="stylesheet">
+
+    <?php
+    if ($_GET["genero"] === "terror") {
+        echo  "<link rel=stylesheet href=estilos_terror.css>";
+    } elseif ($_GET["genero"] === "barbie") {
+        echo "<link rel=stylesheet href=estilos_barbie.css>";
+    }
+    ?>
+
 
 </head>
 
 <body>
     <div class="contenedor">
         <div class="barraNavegacion">
-            <a href="categorias.php">Inicio</a>
+            <a href="categorias.php" class="inicio">Inicio</a>
 
         </div>
         <div class="cuerpo">
 
-            <div class="indiceIzq">Indice izquierdo</div>
-            <div class="datosPag"><?php
-                                    class Pelicula
-                                    {
-                                        public $titulo;
-                                        public $descripcion;
-                                        public $imagen;
-                                        function __construct($titulo, $descripcion, $imagen)
-                                        {
-                                            $this->titulo = $titulo;
-                                            $this->descripcion = $descripcion;
-                                            $this->imagen = $imagen;
-                                        }
+            <div class="indiceIzq"></div>
+            <div class="datosPag">
+                <?php
+                class Pelicula
+                {
+                    public $titulo;
+                    public $descripcion;
+                    public $imagen;
+                    function __construct($titulo, $descripcion, $imagen)
+                    {
+                        $this->titulo = $titulo;
+                        $this->descripcion = $descripcion;
+                        $this->imagen = $imagen;
+                    }
 
-                                        function pintarPeli()
-                                        {
+                    function pintarPeli()
+                    {
 
-                                            echo "<div class='individuales'>";
+                        echo "<div class='individuales'>";
+                        echo "<div class='titulo'>" . $this->titulo . "   </div>";
+                        echo "<p class='votos'>Votos:</p> ";
 
-                                            echo "<div class='imagen'>'<img src=" . $this->imagen . "/div>'  </div>";
-                                            echo "<br/>";
-                                            echo "<div class='titulo'>" . $this->titulo . "   </div>";
-                                            echo "<br/>";
-                                            echo  "<div class='descripcion'>" . $this->descripcion . "  </div>";
-                                            echo "<br/>";
-                                            echo "Duración: ";
-                                            echo "<p class='votos'>Votos:</p> ";
-                                            echo "</div>";
-                                        }
+                        echo "<br/>";
+                        echo "<div >'<img class='imagen'  src=" . $this->imagen . " </div>";
+                        echo "<br/>";
 
-                                        public function getTitulo()
-                                        {
-                                            return $this->titulo;
-                                        }
-                                        public function setTitulo($titulo)
-                                        {
-                                            return $this->titulo;
-                                        }
-                                        public function getImagen()
-                                        {
-                                            return $this->imagen;
-                                        }
-                                        public function setImagen($imagen)
-                                        {
-                                            return $this->titulo;
-                                        }
-                                        public function getDescripcion()
-                                        {
-                                            return $this->titulo;
-                                        }
-                                        public function setDescripcion($descripcion)
-                                        {
-                                            return $this->titulo;
-                                        }
-                                    }
-                                    $peli = new Pelicula("Barbie en La princesa y la costurera", "La mítica muñeca Barbie cobra vida en esta moderna versión del cuento de Mark Twain \"El Príncipe y el Mendigo\" sobre un error de identidad y el poder de la amistad. Con la voz de Gisela como Barbie cantando siete temas originales llega la primera película musical de Barbie interpretando además un doble papel.", "imgs/img1.jpg");
-                                    $peli->pintarPeli();
-                                    $peli = new Pelicula("Barbie en La princesa y la costurera", "La mítica muñeca Barbie cobra vida en esta moderna versión del cuento de Mark Twain \"El Príncipe y el Mendigo\" sobre un error de identidad y el poder de la amistad. Con la voz de Gisela como Barbie cantando siete temas originales llega la primera película musical de Barbie interpretando además un doble papel.", "imgs/img1.jpg");
-                                    $peli->pintarPeli();
-                                    ?></div>
-            <div class="indiceDerecho">Indice derecho</div>
+                        echo  "<div class='descripcion'>" . $this->descripcion . "</div>";
+
+                        echo "<br/>";
+                        echo "<br/>";
+                        echo "Duración: ";
+                        echo "<a href='fichaPelicula.php' class='fichaGrande'> Ver ficha</a> ";
+                        
+                        echo "</div>";
+                    }
+
+                    public function getTitulo()
+                    {
+                        return $this->titulo;
+                    }
+                    public function setTitulo($titulo)
+                    {
+                        return $this->titulo;
+                    }
+                    public function getImagen()
+                    {
+                        return $this->imagen;
+                    }
+                    public function setImagen($imagen)
+                    {
+                        return $this->titulo;
+                    }
+                    public function getDescripcion()
+                    {
+                        return $this->titulo;
+                    }
+                    public function setDescripcion($descripcion)
+                    {
+                        return $this->titulo;
+                    }
+                }
+                $peli = new Pelicula("Barbie en La princesa y la costurera",
+                 "La mítica muñeca Barbie cobra vida en esta moderna versión del cuento de Mark Twain
+                  \"El Príncipe y el Mendigo\" sobre un error de identidad y el poder de la amistad. Con
+                   la voz de Gisela como Barbie cantando siete temas originales llega la primera película
+                    musical de Barbie interpretando además un doble papel.", "imgs/img1.jpg");
+                $peli->pintarPeli();
+                $peli = new Pelicula("Barbie en La princesa y la costurera", "La mítica muñeca Barbie cobra vida en esta moderna versión del cuento de Mark Twain \"El Príncipe y el Mendigo\" sobre un error de identidad y el poder de la amistad. Con la voz de Gisela como Barbie cantando siete temas originales llega la primera película musical de Barbie interpretando además un doble papel.", "imgs/img1.jpg");
+                $peli->pintarPeli();
+                ?></div>
+            <div class="indiceDerecho"></div>
 
 
 
         </div>
-        <div class="piePag">Pie de pagina</div>
+        <div class="piePag"></div>
     </div>
 </body>
 
