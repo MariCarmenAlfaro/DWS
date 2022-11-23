7aebf28bba29751d8bf4baffcd77e07de79b4693
MªCarmenAlfaro <maricarmenalfaro1616@gmail.com>
2022-11-23 18:20:17 +0100

    Arreglo css

--- tema3/practicas/carteleria/categorias.php
+++ tema3/practicas/carteleria/categorias.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categorias</title>
-  
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="estilos_categorias.css">
 </head>
 <body>
@@ -15,7 +18,7 @@
         
     <ul class="listaCategorias">
         <li class="generos"> <a href="peliculas.php?genero=terror">Terror</a> </li>
-        <li class="generos"> <a href="peliculas.php?genero=barbie">Película infantil</a> </li>
+        <li class="generos"> <a href="peliculas.php?genero=barbie">Infantil</a> </li>
     </ul>
     </div>
 </body>
