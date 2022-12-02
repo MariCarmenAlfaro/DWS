086d41dd282762c16a28fbe056021d9a96f3c625
MariCarmenAlfaro <101742672+MariCarmenAlfaro@users.noreply.github.com>
2022-12-02 20:29:08 +0100

    nuevo casa

---
+++ tema3/backPeli.php
@@ -0,0 +1,148 @@
+<?php
+/*$usuario = $_POST["usuario"];
+$password =  $_POST["password"];
+
+
+echo $usuario;
+echo $password;*/
+
+
+contenidoPeli();
+
+
+function contenidoPeli()
+{
+    $conexion = mysqli_connect('localhost', 'root', '1234');
+
+    if (mysqli_connect_errno()) {
+        echo "Error al conectar a MySQL" . mysqli_connect_error();
+    }
+
+    mysqli_select_db($conexion, 'carteleria');
+   
+    
+    
+    $id_categoria =$_GET['genero'];
+    $sanitized_categoria_id = mysqli_real_escape_string($conexion, $id_categoria);
+
+    if ($_GET["genero"] === "2") {
+        $consulta = "SELECT * FROM T_Pelicula  where id_categoria=2;";
+    } elseif ($_GET["genero"] === "1") {
+        $consulta = "SELECT * FROM T_Pelicula where id_categoria=1 ;";
+    }
+  //  $consulta = "SELECT * FROM T_Pelicula  ;";
+  
+   
+    $resultado = mysqli_query($conexion, $consulta);
+
+    if (!$resultado) {
+        $mensaje = 'Consulta invalida: ' . mysqli_error($conexion) . "\n";
+        $mensaje .= 'Consulta realizada: ' . $consulta;
+        die($mensaje);
+    } else 
+    {
+        $arrayPeli = [];
+        $contador = 0;
+        if (($resultado->num_rows) > 0) {
+            while ($registro = mysqli_fetch_assoc($resultado)) {
+              $peli = new Pelicula(
+                    $registro['id'],
+                    $registro['titulo'],
+                    $registro['año'],
+                    $registro['duracion'],
+                    $registro['sinopsis'],
+                    $registro['imagen'],
+                    $registro['votos'],
+                    $registro['id_categoria']
+              );
+              $arrayPeli[] = $peli;
+                pintarPeli($peli);
+            }
+        
+          
+        } else {
+            echo "No hay resultados";
+        }
+    }
+    
+
+    
+}
+function pintarPeli($peli)
+    {
+  echo "<div class='individuales'>";
+ 
+
+  echo "<div class='titulo'>" . $peli->titulo . "   </div>";
+
+  echo "<p class='votos'>Votos:". $peli->votos."</p> ";
+
+  echo "<br/>";
+
+  echo "<div ><img class='imagen'  src=" . $peli->imagen . ">
+                                      </div>";
+
+  echo "<br/>";
+
+  echo  "<div class='descripcion'>" . $peli->sinopsis . "</div>";
+
+  echo "<br/>";
+  echo "<br/>";
+  echo "Duración: ".$peli->duracion;
+  echo "<a href='fichaPelicula.php' class='fichaGrande'> Ver ficha</a> ";
+
+  echo "</div>";
+    }
+
+
+class Pelicula
+{
+    public $id;
+    public $titulo;
+    public $año;
+    public $duracion;
+    public $sinopsis;
+    public $imagen;
+    public $votos;
+    public $idCategoria;
+    function __construct($id, $titulo, $año, $duracion, $sinopsis, $imagen, $votos, $idCategoria)
+    {
+        $this->id = $id;
+        $this->titulo = $titulo;
+        $this->año = $año;
+        $this->duracion = $duracion;
+        $this->sinopsis = $sinopsis;
+        $this->imagen = $imagen;
+        $this->votos = $votos;
+        $this->idCategoria = $idCategoria;
+        //contenidoPeli();
+    }
+    
+  
+
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+    public function setTitulo($titulo)
+    {
+        return $this->titulo;
+    }
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+    public function setImagen($imagen)
+    {
+        return $this->titulo;
+    }
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+    public function setDescripcion($descripcion)
+    {
+        return $this->descripcion;
+    }
+}
+
---
+++ tema3/peliculas.php
@@ -0,0 +1,60 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelicula</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Metal+Mania&family=Sevillana&display=swap" rel="stylesheet">
+
+    <?php
+    if ($_GET["genero"] === "2") {
+        echo  "<link rel=stylesheet href=estilos_terror.css>";
+    } elseif ($_GET["genero"] === "1") {
+        echo "<link rel=stylesheet href=estilos_barbie.css>";
+    }else{
+        header('categorias.php');
+    }
+    ?>
+
+
+</head>
+
+<body>
+    <div class="contenedor">
+        <div class="barraNavegacion">
+            <ul>
+                <li class="listaNavegacion">
+                    <a href="categorias.php" class="inicio">Inicio</a>
+                </li>
+                <li class="listaNavegacion">
+                    <a href="categorias.php" class="inicio">Ordenar por votos</a>
+                </li>
+                <li class="listaNavegacion">
+                    <a href="categorias.php" class="inicio">Ordenar por título</a>
+                </li>
+            </ul>
+        </div>
+        <div class="cuerpo">
+
+            <?php
+
+            require('backPeli.php');
+
+           // contenidoPeli();
+    
+            ?>
+
+
+
+
+
+
+        </div>
+        <div class="piePag">CopyRight</div>
+    </div>
+</body>
+
+</html>
